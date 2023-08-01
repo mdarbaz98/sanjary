dd232c6bf6f4752b1cd9df13930d2ad1e687c7b0
mehtabq24 <mehtabq24@gmail.com>
2023-08-01 23:21:04 +0530

    test

--- include/header.php
+++ include/header.php
@@ -1,4 +1,4 @@
-<?php $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]"; 
+<?php echo $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]"; 
 //include('include/database.php');
 
 ?>
