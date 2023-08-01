f667b66dc47cbc56a9e6a2f20631e33228a5c43d
mehtabq24 <mehtabq24@gmail.com>
2023-08-02 00:10:07 +0530

    sanjary

--- sanjary-admin/include/config.php
+++ sanjary-admin/include/config.php
@@ -7,8 +7,8 @@ if(strpos($actual_link, 'localhost')) {
 	$db_password="";
 }else {
 	$db_host="localhost"; 
-	$db_user="sanjaryf_admin_sanjary	";
-	$db_name="sanjaryf_admin_sanjary	";
+	$db_user="sanjaryf_admin_sanjary";
+	$db_name="sanjaryf_admin_sanjary";
 	$db_password="}bDf6hr7}QfI";
 }
 
@@ -16,7 +16,7 @@ try
 {
 	$conn=new PDO("mysql:host={$db_host};dbname={$db_name};port=3306",$db_user,$db_password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "connected";
+	echo "connected":
 }
 catch(PDOEXCEPTION $e)
 {
