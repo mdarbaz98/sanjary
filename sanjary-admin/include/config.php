d63689228e6abf9e6e9350b1d600cb9a32acfa6a
mehtabq24 <mehtabq24@gmail.com>
2023-08-02 00:17:30 +0530

    test conn

--- sanjary-admin/include/config.php
+++ sanjary-admin/include/config.php
@@ -1,16 +1,16 @@
 <?php
-$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-if(strpos($actual_link, 'localhost')) {
-	$db_host="localhost"; 
-	$db_user="root";
-	$db_name="sanjary";
-	$db_password="";
-}else {
+// $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+// if(strpos($actual_link, 'localhost')) {
+// 	$db_host="localhost"; 
+// 	$db_user="root";
+// 	$db_name="sanjary";
+// 	$db_password="";
+// }else {
 	$db_host="localhost"; 
 	$db_user="sanjaryf_sanjaryf_final_user";
 	$db_name="sanjaryf_final_database";
 	$db_password="4FIK3ck2mP@P";
-}
+// }
 
 try
 {
