22621e59c442f8bd9929862b0a51e00bca668487
mehtabq24 <mehtabq24@gmail.com>
2023-08-01 23:12:24 +0530

    testing

--- sanjary-admin/index.php
+++ sanjary-admin/index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<title>Sanjary PANEL</title>
+	<title>Sanjary PANEL mehtab</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta content="AUGS Admin & Dashboard" name="description" />
 	<meta content="Themesbrand" name="author" />
