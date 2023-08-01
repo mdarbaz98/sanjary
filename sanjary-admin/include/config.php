633b4f02fd5cfdb3f2fef08c355a773e9a05b5b1
mehtabq24 <mehtabq24@gmail.com>
2023-08-01 23:10:12 +0530

    check

--- sanjary-admin/include/config.php
+++ sanjary-admin/include/config.php
@@ -16,6 +16,7 @@ try
 {
 	$conn=new PDO("mysql:host={$db_host};dbname={$db_name};port=3306",$db_user,$db_password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "connected";
 }
 catch(PDOEXCEPTION $e)
 {
