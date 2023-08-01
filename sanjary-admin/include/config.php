36b754ea7b5092cfafd5a341aa4afde56d6d7f67
mehtabq24 <mehtabq24@gmail.com>
2023-08-02 00:51:52 +0530

    db test

--- sanjary-admin/include/config.php
+++ sanjary-admin/include/config.php
@@ -1,22 +1,22 @@
 <?php
-// $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
-// if(strpos($actual_link, 'localhost')) {
-// 	$db_host="localhost"; 
-// 	$db_user="root";
-// 	$db_name="sanjary";
-// 	$db_password="";
-// }else {
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+if(strpos($actual_link, 'localhost')) {
+	$db_host="localhost"; 
+	$db_user="root";
+	$db_name="sanjary";
+	$db_password="";
+}else {
 	$db_host="localhost"; 
 	$db_user="sanjaryf_sanjaryf_final_user";
 	$db_name="sanjaryf_final_database";
 	$db_password="4FIK3ck2mP@P";
-// }
+}
 
 try
 {
 	$conn=new PDO("mysql:host={$db_host};dbname={$db_name};port=3306",$db_user,$db_password);
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "connected":
+	echo "connected";
 }
 catch(PDOEXCEPTION $e)
 {
